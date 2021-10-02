876f89ed9b726e2212d4196adb61a4b2655017f8
PequenoPreto <lucas.matulis@gmail.com>
2021-10-02 12:12:10 -0300

    Botão e algumas coisas basicas na porta

---
+++ Assets/Botao.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Botao : MonoBehaviour
+{
+    public bool ativo=false;
+    public Animator anim;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+       if(col.gameObject.tag=="pedra")
+       {
+           ativo=true;
+           anim.SetTrigger("Pre");
+       } 
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+       if(col.gameObject.tag=="pedra")
+       {
+           ativo=false;
+           anim.SetTrigger("Normal");
+       } 
+    }
+}
---
+++ Assets/Scripts/Objetos/Porta.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Porta : MonoBehaviour
+{
+    public Animator anim;
+    public Botao botao;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
